0019dc055093ece2c1e3f57307f8638cae1d624c
jhyu <jhyu@sysnew.com>
2020-07-16 21:19:23 +0800

    commit recent changes.

--- config.js
+++ config.js
@@ -7,16 +7,29 @@
  */
 const path = require('path');
 
+const accessDB = {
+  dev: {
+    host: '10.200.61.139:60001',
+    user: 'bbl_db',
+    password: 'OStem@00',
+    database: 'storeDB'
+  },
+  test: {
+    host: 'localhost:3310',
+    user: 'root',
+    password: '13kf7AZvQId@m9Hq',
+    database: 'storeDB'
+  }
+};
+
+const config = {
+  connectionLimit: 10,
+};
+
 module.exports = {
   Port: 3000, // 启动端口
   staticDir: path.resolve('./public'), // 静态资源路径
   uploadDir: path.join(__dirname, path.resolve('public/')), // 上传文件路径
   // 数据库连接设置
-  dbConfig: {
-    connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'storeDB'
-  }
+  dbConfig: Object.assign(config, accessDB['dev'])
 }
